601241d4904595d018f8074b51f568372ae8909a
Siddharth Hariprasad <siddhuhariprasad@gmail.com>
2021-03-26 23:23:07 +0530

    First Commit

---
+++ server/index.js
@@ -0,0 +1,23 @@
+import express from 'express';
+//import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+
+import postRoutes from './routes/posts.js';
+
+const app = express();
+
+app.use('/posts', postRoutes);
+
+app.use(express.json({ limit: "30mb", extended: true }));
+app.use(express.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+const CONNECTION_URL = 'mongodb+srv://sid:Siddhu99@cluster0.43ns8.mongodb.net/memories?retryWrites=true&w=majority'
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(PORT, () => console.log(`Server running on PORT ${PORT}`)))
+    .catch((error) => console.log(error.message));
+
+mongoose.set('useFindAndModify', false);
